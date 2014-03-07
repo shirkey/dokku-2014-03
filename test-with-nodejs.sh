1cd5f78bde8a6f0456774054044382a83f49ef3e
Shirkey <dev@shirkey.me>
2014-03-07 23:09:30 +0700

    changed name of node app

--- test-with-nodejs.sh
+++ test-with-nodejs.sh
@@ -14,18 +14,18 @@ curl localhost:5000
 ecow "Kill the local process and delete the pid as we get ready to deploy to Dokku"
 echo "kill $(cat ./this.pid); rm ./this.pid"
 kill $(cat ./this.pid); rm ./this.pid
-ecow "Create a new remote git repository named dokku that points to dokku@mainga.me:nodehello"
-echo "git remote dokku dokku@mainga.me:nodehello"
-git remote add dokku dokku@mainga.me:nodehello
+ecow "Create a new remote git repository named dokku that points to dokku@mainga.me:nodetest"
+echo "git remote dokku dokku@mainga.me:nodetest"
+git remote add dokku dokku@mainga.me:nodetest
 ecow "Push the local master branch to dokku"
 echo "git push dokku master"
 git push dokku master
-ecow "Test the remote deployment at nodehello.mainga.me"
-echo "curl nodehello.mainga.me"
-curl nodehello.mainga.me
+ecow "Test the remote deployment at nodetest.mainga.me"
+echo "curl nodetest.mainga.me"
+curl nodetest.mainga.me
 ecow "Update the content of the node app, commit and redeploy"
 echo "sed -i \"s/World/Dokku/\" /tmp/node-js-sample/web.js"
 sed -i "s/World/Dokku/" /tmp/node-js-sample/web.js
 echo "git commit -am 'Change greeting'; git push dokku master"
 git commit -am "Change greeting"; git push dokku master
-curl nodehello.mainga.me
+curl nodetest.mainga.me
