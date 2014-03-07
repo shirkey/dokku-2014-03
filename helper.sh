7f7a1a2d5d0e2ef0ebe952ce03daf18b882c31af
Shirkey <dev@shirkey.me>
2014-03-07 16:48:18 +0700

    checkpoint

---
+++ helper.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+export dokku_domain_name="mainga.me"
+
+function ecow(){
+    echo "$@" | cowsay -W 80 -f tux
+}
+function parse_yaml {
+   local prefix=$2
+   local s='[[:space:]]*' w='[a-zA-Z0-9_]*' fs=$(echo @|tr @ '\034')
+   sed -ne "s|^\($s\)\($w\)$s:$s\"\(.*\)\"$s\$|\1$fs\2$fs\3|p" \
+        -e "s|^\($s\)\($w\)$s:$s\(.*\)$s\$|\1$fs\2$fs\3|p"  $1 |
+   awk -F$fs '{
+      indent = length($1)/2;
+      vname[indent] = $2;
+      for (i in vname) {if (i > indent) {delete vname[i]}}
+      if (length($3) > 0) {
+         vn=""; for (i=0; i<indent; i++) {vn=(vn)(vname[i])("_")}
+         printf("%s%s%s=\"%s\"\n", "'$prefix'",vn, $2, $3);
+      }
+   }'
+}
+
+export do_client_key=$(parse_yaml ~/.tugboat | grep client_key | cut -d"=" -f2 | tr -d '"')
+export do_api_key=$(parse_yaml ~/.tugboat | grep api_key | cut -d"=" -f2 | tr -d '"')
---
+++ setup-server-at-digitalocean.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+. ./helper.sh
+
+ecow "Creating a request for a new server instance on DigitalOcean, and wait for completion (typically <1 min)"
+tugboat create $dokku_domain_name
+tugboat wait $dokku_domain_name
+dokku_server_info=$(tugboat info $dokku_domain_name)
+ecow $dokku_server_info
+ecow "Now that the instance is created and running, we can see the info (including IP address):\n$dokku_server_info"
+dokku_server_ip=$(echo $dokku_server_info | grep IP | cut -d':' -f2 | tr -d ' ')
+ecow "Generate the DNS entries for $dokku_domain_name pointing to the new instance"
+curl "https://api.digitalocean.com/domains/new?client_id=$DIGITAL_OCEAN_CLIENT_ID&api_key=$DIGITAL_OCEAN_API_KEY&name=$dokku_domain_name&ip_address=$dokku_server_ip"
+echo $(curl "https://api.digitalocean.com/domains/$dokku_domain_name/records/new?client_id=$DIGITAL_OCEAN_CLIENT_ID&api_key=$DIGITAL_OCEAN_API_KEY&record_type=A&data=$dokku_server_ip")
+echo $(curl "https://api.digitalocean.com/domains/$dokku_domain_name/records/new?client_id=$DIGITAL_OCEAN_CLIENT_ID&api_key=$DIGITAL_OCEAN_API_KEY&record_type=A&data=$dokku_server_ip&name=@")
+ping_result=$(ping $dokku_domain_name)
+ecow "Ping to $dokku_domain_name and confirm name resolution to the new instance:\n$ping_result"
+echo
+ecow "Finally after the demo, we will cleanup by destroying the instance -- hit any key to proceed"
+read
+tugboat destroy $dokku_domain_name
